60e54cfdee7650e540ba19096ff99956ace0dd98
Diza Pointman <bifiroll86@googlemail.com>
2023-12-13 17:20:34 +0100

    text extraction

--- reading.py
+++ reading.py
@@ -2,14 +2,33 @@ from pathlib import Path
 from pypdf import PdfReader
 
 
-pdf_path = (Path.home()/"Projects/VSCode_Projects/Myfuture/PaperTiger/test_pdfs"
-            /"Pride_and_Prejudice.pdf")
+#pdf_path = (Path.home()/"Projects/VSCode_Projects/Myfuture/PaperTiger/test_pdfs" /"Pride_and_Prejudice.pdf")
 
-def print_path():
-    print(pdf_path)
+pdf_path = (Path.home()/"Projects/VSCode_Projects/Myfuture/PaperTiger/test_pdfs" /"antrag-sgb2_ba042689.pdf")
+    
 
 pdf_reader = PdfReader(pdf_path)
 
+def pdf_extract_metadata():
+    print("Printing Path")
+    print(pdf_path)    
+    print("Printing Pages")
+    print(pdf_reader.pages)
+    print("Printing Metadata as a whole")
+    print(pdf_reader.metadata)
+    print("Printing extracted Metadata")
+    
+    for x in pdf_reader.metadata:
+        # getting all the metadata
+        print(f"{x[1:]}...: " + pdf_reader.metadata.get(f"{x}", ""))
+        
+
+def pdf_extract_data():
+    for page in pdf_reader.pages:
+        print("-----new page-----")
+        print(page.extract_text())
+
 if __name__ == '__main__':
-    print_path()
-    print(len(pdf_reader.pages))
+    
+    #pdf_extract_metadata()
+    pdf_extract_data()
